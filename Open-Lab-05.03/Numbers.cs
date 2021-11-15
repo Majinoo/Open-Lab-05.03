205cf2fdca4bfc2b2daeb4590493af77094c8fa4
Majinoo <91146809+Majinoo@users.noreply.github.com>
2021-11-15 16:08:02 +0100

    I implemented the method that turns an integer into a string formatted to thousands.

--- Open-Lab-05.03/Numbers.cs
+++ Open-Lab-05.03/Numbers.cs
@@ -1,12 +1,23 @@
-﻿using System;
-
-namespace Open_Lab_05._03
+﻿namespace Open_Lab_05._03
 {
     public class Numbers
     {
         public string FormatNum(int num)
         {
-            throw new NotImplementedException();
+            string number = num.ToString();
+            if (num > 999 && num < 1000000)
+            {
+                number = number.Insert(number.Length - 3, ",");
+                return number;
+            }
+            else if(num < 999)
+            {
+                return num.ToString();
+            }
+            else
+            {
+                return num.ToString("N0");
+            }
         }
     }
 }
